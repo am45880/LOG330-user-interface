eb63828901835583bfff4b056c189ff02674b209
Yassine Rachid <am45880@etsmtl.net>
2017-03-07 19:30:02 -0500

    other files not commited

---
+++ src/app/pages/statistique-page/statistique-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistique-page',
+  templateUrl: './statistique-page.component.html',
+  styleUrls: ['./statistique-page.component.css']
+})
+export class StatistiquePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
